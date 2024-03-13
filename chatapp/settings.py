194997b0ecc8b9d8cbad40d380d6e47c5eb97b70
Prasangati <53667641+Prasangati@users.noreply.github.com>
2024-03-13 02:10:32 -0400

    Hopefully it all works

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -97,10 +97,7 @@ CHANNEL_LAYERS = {
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 
