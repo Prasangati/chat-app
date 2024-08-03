f31294d158bbc83b394fb425b35bb1962dcfdbe6
Prasangati <53667641+Prasangati@users.noreply.github.com>
2024-08-02 23:49:52 -0400

    Inquiry feature is causing problems. Email is not being sent properly.

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.mainpage,name='mainpage'),
-    path('about/',views.enquiry_view,name='about_enquiry'),
+    path('about/',views.about_enquiry_view,name='about_enquiry'),
     path('signup/',views.signup, name='signup'),
     path('logout/', views.custom_logout_view,name='logout'),
     path('login/', LoginView.as_view(template_name='core/login.html'),name='login'),
