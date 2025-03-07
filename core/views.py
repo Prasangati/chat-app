22dd61781216adc37a774d5d1748185bcef2f0ca
prasangatiwari <prasangahere@gmail.com>
2025-03-07 13:38:07 -0500

    Authetication

--- core/views.py
+++ core/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 # Create your views here.
 
+
+def mainpage(request):
+    return render(request, 'core/front.html')
 def about_enquiry_view(request):
     if request.method == 'POST':
         form = EnquiryForm(request.POST)
